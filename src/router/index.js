d351967a46ad716baacac2728f9fe9395dbf028d
leozhou <492753662@qq.com>
2017-05-19 11:25:35 +0800

    update components

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '../components/Hello'
-import CarAntiTheft from '../components/CarAntiTheft'
-import House from '../components/House'
-import LastPage from '../components/LastPage'
-import ProjectManage from '../components/ProjectManage'
-import RunDate from '../components/RunDate'
-import SPDR from '../components/SPDR'
+import Hello from '@/components/Hello'
+import CarAntiTheft from '@/components/CarAntiTheft'
+import House from '@/components/House'
+import LastPage from '@/components/LastPage'
+import ProjectManage from '@/components/ProjectManage'
+import RunDate from '@/components/RunDate'
+import SPDR from '@/components/SPDR'
 
 Vue.use(Router)
 
