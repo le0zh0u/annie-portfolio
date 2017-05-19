9ff609fe9cbe426a093c5ce8ee93afaac3b7227d
leozhou <492753662@qq.com>
2017-05-19 11:13:08 +0800

    update annie protfolio

---
+++ src/components/Hello.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="hello">
+    <el-row :gutter="20" class="mt-20">
+      <el-col :span="6">
+        <el-card :body-style="{ padding: '0px' }">
+          <router-link to="/s-house">
+            <div class="img-box">
+              <img src="http://oiuhevdur.bkt.clouddn.com/1sHouse-new.jpg" class="image">
+            </div>
+          </router-link>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card :body-style="{ padding: '0px' }">
+          <router-link to="/sprd">
+            <div class="img-box">
+              <img src="http://oiuhevdur.bkt.clouddn.com/2spdr.jpg" class="image">
+            </div>
+          </router-link>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card :body-style="{ padding: '0px' }">
+          <router-link to="/car-anti-theft">
+            <div class="img-box">
+              <img src="http://oiuhevdur.bkt.clouddn.com/3%E8%BD%A6%E9%98%B2%E7%9B%97new.jpg" class="image">
+            </div>
+          </router-link>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card :body-style="{ padding: '0px' }">
+          <router-link to="/project-manage">
+            <div class="img-box">
+              <img src="http://oiuhevdur.bkt.clouddn.com/4project-manage-new.jpg" class="image">
+            </div>
+          </router-link>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20" class="mt-20">
+      <el-col :span="6">
+        <el-card :body-style="{ padding: '0px' }">
+          <router-link to="/run-date">
+            <div class="img-box">
+              <img src="http://oiuhevdur.bkt.clouddn.com/5run-date-new.jpg" class="image">
+            </div>
+          </router-link>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card :body-style="{ padding: '0px' }">
+          <router-link to="/last-page">
+            <div class="img-box">
+              <img src="http://oiuhevdur.bkt.clouddn.com/last-page-zjj.jpg" class="image">
+            </div>
+          </router-link>
+        </el-card>
+      </el-col>
+
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        currentDate: new Date()
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .img-box {
+    height: 180px
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .mt-20{
+    margin-top: 20px
+  }
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import CarAntiTheft from '@/components/CarAntiTheft'
+import House from '@/components/House'
+import LastPage from '@/components/LastPage'
+import ProjectManage from '@/components/ProjectManage'
+import RunDate from '@/components/RunDate'
+import SPDR from '@/components/SPDR'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/car-anti-theft',
+      name: 'carAntiTheft',
+      component: CarAntiTheft
+    },
+    {
+      path: '/last-page',
+      name: 'lastPage',
+      component: LastPage
+    },
+    {
+      path: '/project-manage',
+      name: 'projectManage',
+      component: ProjectManage
+    },
+    {
+      path: '/run-date',
+      name: 'runDate',
+      component: RunDate
+    },
+    {
+      path: '/SPDR',
+      name: 'SPDR',
+      component: SPDR
+    },
+    {
+      path: '/s-house',
+      name: 'house',
+      component: House
+    }
+  ]
+})
