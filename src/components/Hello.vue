2fb521667d4945a241318dbd734af0e97800d5f0
leozhou <492753662@qq.com>
2017-08-02 17:31:45 +0800

    for response page

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,8 +5,8 @@
     <div><span>越努力，越幸运</span></div>
   </div>
   <div class="content">
-    <el-row :gutter="40" v-for="(cards, i) in cardList" :key="i" class="mt-30">
-      <el-col :span="6" v-for="(card, index) in cards" :key="index">
+    <el-row :gutter="40" v-for="(cards, i) in resetCardList(cardList)" :key="i" class="mt-30">
+      <el-col :xs="24" :sm="12" :md="8" :lg="6" v-for="(card, index) in cards" :key="index">
         <el-card :body-style="{ padding: '0px' }" class="card-box">
           <router-link :to="card.linkTo">
             <div>
@@ -48,52 +48,109 @@ export default {
   data() {
     return {
       currentDate: new Date(),
-      cardList: [
-        [{
-          id: 1,
-          img: 'http://oiuhevdur.bkt.clouddn.com//home1.png',
-          name: '运动社交类——约跑',
-          linkTo: '/run-date',
-          category: 'APP设计'
-        }, {
-          id: 2,
-          img: 'http://oiuhevdur.bkt.clouddn.com//home2.png',
-          name: '保镖云防盗V2.0.0',
-          linkTo: '/car-anti-theft',
-          category: 'UI设计'
-        }, {
-          id: 3,
-          img: 'http://oiuhevdur.bkt.clouddn.com//home3.png',
-          name: 'Design项目管理平台',
-          linkTo: '/project-manage',
-          category: 'APP设计'
-        }, {
-          id: 4,
-          img: 'http://oiuhevdur.bkt.clouddn.com//home4.png',
-          name: '每刻官网V1.0',
-          linkTo: '/maycur-website',
-          category: 'WEB设计'
-        }],
-        [{
-          id: 5,
-          img: 'http://oiuhevdur.bkt.clouddn.com//home5.png',
-          name: '线上BANNER规范',
-          linkTo: '/banner-guide',
-          category: 'banner规范设计'
-        }, {
-          id: 6,
-          img: 'http://oiuhevdur.bkt.clouddn.com//home6.png',
-          name: '视觉绘制',
-          linkTo: '/hand-drawing',
-          category: '插画'
-        }, {
-          id: 7,
-          img: 'http://oiuhevdur.bkt.clouddn.com//home7.png',
-          name: 'Ring Clean',
-          linkTo: '/ring-clean',
-          category: '概念设计'
-        }]
-      ]
+      screenWidth: document.body.clientWidth,
+      cardList: [{
+        id: 1,
+        img: 'http://oiuhevdur.bkt.clouddn.com//home1.png',
+        name: '运动社交类——约跑',
+        linkTo: '/run-date',
+        category: 'APP设计'
+      }, {
+        id: 2,
+        img: 'http://oiuhevdur.bkt.clouddn.com//home2.png',
+        name: '保镖云防盗V2.0.0',
+        linkTo: '/car-anti-theft',
+        category: 'UI设计'
+      }, {
+        id: 3,
+        img: 'http://oiuhevdur.bkt.clouddn.com//home3.png',
+        name: 'Design项目管理平台',
+        linkTo: '/project-manage',
+        category: 'APP设计'
+      }, {
+        id: 4,
+        img: 'http://oiuhevdur.bkt.clouddn.com//home4.png',
+        name: '每刻官网V1.0',
+        linkTo: '/maycur-website',
+        category: 'WEB设计'
+      }, {
+        id: 5,
+        img: 'http://oiuhevdur.bkt.clouddn.com//home5.png',
+        name: '线上BANNER规范',
+        linkTo: '/banner-guide',
+        category: 'banner规范设计'
+      }, {
+        id: 6,
+        img: 'http://oiuhevdur.bkt.clouddn.com//home6.png',
+        name: '视觉绘制',
+        linkTo: '/hand-drawing',
+        category: '插画'
+      }, {
+        id: 7,
+        img: 'http://oiuhevdur.bkt.clouddn.com//home7.png',
+        name: 'Ring Clean',
+        linkTo: '/ring-clean',
+        category: '概念设计'
+      }]
+    }
+  },
+  methods: {
+    resetCardList() {
+      let result = []
+      let list = this.cardList;
+      let cards  = [];
+      for (var i = 0; i < list.length; i++) {
+        if (this.screenWidth >= 1200  ) {
+          if (cards.length == 4) {
+            result.push(cards);
+            cards = [];
+          }
+          cards.push(list[i]);
+        }else if (this.screenWidth >= 976) {
+          if (cards.length == 3) {
+            result.push(cards);
+            cards = [];
+          }
+          cards.push(list[i]);
+        }else if (this.screenWidth >= 753) {
+          if (cards.length == 2) {
+            result.push(cards);
+            cards = [];
+          }
+          cards.push(list[i]);
+        }else{
+          if (cards.length == 1) {
+            result.push(cards);
+            cards = [];
+          }
+          cards.push(list[i]);
+        }
+      }
+      if (cards.length !=0) {
+        result.push(cards);
+      }
+      return result;
+    }
+  },
+  mounted() {
+    const that = this
+    window.onresize = () => {
+      return (() => {
+        window.screenWidth = document.body.clientWidth
+        that.screenWidth = window.screenWidth
+      })()
+    }
+  },
+  watch: {
+    screenWidth(val) {
+      if (!this.timer) {
+        this.screenWidth = val
+        this.timer = true
+        let that = this
+        setTimeout(function() {
+          that.timer = false
+        }, 400)
+      }
     }
   }
 }
@@ -131,21 +188,23 @@ export default {
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .48);
 }
 
-.card-content{
+.card-content {
   padding: 20px;
   text-align: left;
 }
-a{
+
+a {
   text-align: center;
   color: #2c3e50;
   text-decoration: none;
 }
+
 .card-name {
   font-family: PingFangSC-Regular;
   font-size: 16px;
 }
 
-.secondary{
+.secondary {
   margin-top: 15px;
 }
 
@@ -162,6 +221,10 @@ a{
 
 .mt-30 {
   margin-top: 30px
+}
+
+.clear {
+  clear: both;
 }
 
 .footer {
