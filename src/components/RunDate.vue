4e84079be6ea70b2ac84e23a2f9d2c82b0e10746
leozhou <492753662@qq.com>
2017-08-04 15:11:41 +0800

    update run date

--- src/components/RunDate.vue
+++ src/components/RunDate.vue
@@ -2,30 +2,15 @@
   <div>
     <img src="http://oiuhevdur.bkt.clouddn.com/portfolio/detail/run-1.png" class="image">
     <img src="http://oiuhevdur.bkt.clouddn.com/portfolio/detail/run-2.png" class="image">
-    <my-video :sources="video.sources" :options="video.options"></my-video>
+
   </div>
 </template>
 <script>
-  import myVideo from 'vue-video'
+  
   export default {
     name: 'run-date-page',
     data () {
-      return {
-        video: {
-                sources: [{
-                    src: 'http://covteam.u.qiniudn.com/oceans.mp4',
-                    type: 'video/mp4'
-                }],
-                options: {
-                    autoplay: true,
-                    volume: 0.6,
-                    poster: 'http://covteam.u.qiniudn.com/poster.png'
-                }
-            }
-      }
-    },
-    components: {
-        myVideo
+      return {}
     }
   }
 </script>
